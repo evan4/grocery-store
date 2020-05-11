902bb303b775fdda989748e2063642bd1bda5da7
Ivan <evan4mc@gmail.com>
2020-05-11 16:38:35 +0500

    search

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -24,7 +24,6 @@ class CategoryController extends AppController
       $category->description
     );
 
-
     $query = Product::find()->where(['category_id' => $id]);
     $countQuery = clone $query;
     $pages = new Pagination([
@@ -38,5 +37,35 @@ class CategoryController extends AppController
         ->all();
 
     return $this->render('view', compact('products', 'category', 'pages'));
+  }
+
+  public function actionSearch()
+  {
+    $q = trim(Yii::$app->request->get('q'));
+    
+    $this->setMeta(
+      "Поиск {$q} :: ". Yii::$app->name
+    );
+
+    if (!$q) {
+      return $this->render('search');
+    }
+
+    $query = Product::find()->where(['ILIKE', 'title', $q]);
+    
+    $countQuery = clone $query;
+    $pages = new Pagination([
+      'totalCount' => $countQuery->count(),
+      'pageSize' => 3,
+      'forcePageParam' => false,
+      'pageSizeParam' => false
+    ]);
+
+    $products = $query->offset($pages->offset)
+        ->limit($pages->limit)
+        ->all();
+     
+    return $this->render('search', compact('products', 'pages', 'q'));
+
   }
 }
--- views/layouts/grocery.php
+++ views/layouts/grocery.php
@@ -2,6 +2,7 @@
 
 use app\assets\AppAsset;
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 AppAsset::register($this);
 ?>
@@ -31,8 +32,9 @@ License URL: http://creativecommons.org/licenses/by/3.0/
         <a href="products.html">Today's special Offers !</a>
     </div>
     <div class="w3l_search">
-        <form action="#" method="post">
-            <input type="text" name="Product" value="Search a product..." onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search a product...';}" required="">
+        <form action="<?=Url::to('/search'); ?>" method="get">
+            <input type="text" name="q" 
+            placeholder="Search a product..." required="">
             <input type="submit" value=" ">
         </form>
     </div>
